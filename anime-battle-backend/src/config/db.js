6093a71e4ff23b3ad670439a58878382ba387093
Casolin <animecaster@gmail.com>
2026-01-02 00:16:11 +0100

    xqc

--- anime-battle-backend/src/config/db.js
+++ anime-battle-backend/src/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_URL);
     console.log("Database connected");
   } catch (error) {
     console.error(error.message);

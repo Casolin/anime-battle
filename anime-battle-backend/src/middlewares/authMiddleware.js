cf017c62a80f9ed84c5a57e72532d7c003b894bb
Casolin <animecaster@gmail.com>
2026-01-02 00:36:42 +0100

    xqc

--- anime-battle-backend/src/middlewares/authMiddleware.js
+++ anime-battle-backend/src/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
+import User from "../models/User.js";
 import { verifyToken } from "../utils/jwt.js";
 
-export const authenticate = (req, res, next) => {
+export const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -9,9 +10,16 @@ export const authenticate = (req, res, next) => {
 
     const token = authHeader.split(" ")[1];
     const decoded = verifyToken(token);
-    req.user = decoded;
+
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
     next();
   } catch (err) {
+    console.error("Auth error:", err);
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
